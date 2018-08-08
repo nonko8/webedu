20ea38b64e82e0eda60a86e18a6fb9a56e197a24
nonko8 <git@nonko8.com>
2018-08-08 23:19:28 +0900

    add factorial function fixed typo

--- node-js/app.js
+++ node-js/app.js
@@ -27,7 +27,7 @@ console.log("すべてのテストを通過しました");
  */
 function factorial(n) {
     let result = 1;
-    for (let i = 1; i <= n, i++) {
+    for (let i = 1; i <= n; i++) {
         result = result * i;
     }
     return result;
