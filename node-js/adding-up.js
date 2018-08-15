3598a3e4fcf29f2355316fc2d23afc7fd3ce83a1
nonko8 <git@nonko8.com>
2018-08-15 23:28:28 +0900

    出力データの整形

--- node-js/adding-up.js
+++ node-js/adding-up.js
@@ -40,6 +40,10 @@ rs.on('close', () => {
     const rankingArray = Array.from(map).sort((pair1, pair2) => {
         return pair2[1].change - pair1[1].change;
     })
+    const rankingStrings = rankingArray.map((pair) => {
+        return pair[0] + ": " + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
+    })
     //console.log(map);
-    console.log(rankingArray);
+    //console.log(rankingArray);
+    console.log(rankingStrings);
 })
